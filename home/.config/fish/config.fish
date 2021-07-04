7bd77a2c406fb4f32a865a5d04028d2aa23c84a0
Felipe Morales <hel.sheep@gmail.com>
2021-07-04 11:14:05 -0400

    update fish config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,3 +2,12 @@ set fish_greeting
 
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
+source /etc/grc.fish
+
+alias ls='grc --colour=auto ls --color=always -C'
+
+fundle plugin 'oh-my-fish/plugin-foreign-env'
+fundle plugin 'sentriz/fish-pipenv'
+fundle plugin 'victante/trish'
+
+fundle init
